ae27bd8fa14d649a8bd65ba8ca2ad224a5c0c065
Jose <jose.ale.gt.umg@gmail.com>
2020-04-29 12:36:25 -0600

    mantenimientos

---
+++ Examen_P2/Capa_Diseño/Mantenimientos/Frm_Mantcurso.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Capa_Diseño.Mantenimientos
+{
+    partial class Frm_Mantcurso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Mantcurso));
+            this.Cbo_estado = new System.Windows.Forms.ComboBox();
+            this.Estado = new System.Windows.Forms.Label();
+            this.Txt_nombre = new System.Windows.Forms.TextBox();
+            this.Txt_Codigo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Pnl_nombreForm = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Ayuda = new System.Windows.Forms.Button();
+            this.btn_minimizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            this.Btn_consultar = new System.Windows.Forms.Button();
+            this.Btn_borrar = new System.Windows.Forms.Button();
+            this.Btn_guardar = new System.Windows.Forms.Button();
+            this.Btn_editar = new System.Windows.Forms.Button();
+            this.Btn_ingresar = new System.Windows.Forms.Button();
+            this.Pnl_nombreForm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Cbo_estado
+            // 
+            this.Cbo_estado.FormattingEnabled = true;
+            this.Cbo_estado.Items.AddRange(new object[] {
+            "Activo",
+            "Inactivo"});
+            this.Cbo_estado.Location = new System.Drawing.Point(464, 284);
+            this.Cbo_estado.Name = "Cbo_estado";
+            this.Cbo_estado.Size = new System.Drawing.Size(121, 24);
+            this.Cbo_estado.TabIndex = 120;
+            // 
+            // Estado
+            // 
+            this.Estado.AutoSize = true;
+            this.Estado.Location = new System.Drawing.Point(405, 284);
+            this.Estado.Name = "Estado";
+            this.Estado.Size = new System.Drawing.Size(52, 17);
+            this.Estado.TabIndex = 119;
+            this.Estado.Text = "Estado";
+            // 
+            // Txt_nombre
+            // 
+            this.Txt_nombre.Location = new System.Drawing.Point(725, 223);
+            this.Txt_nombre.Name = "Txt_nombre";
+            this.Txt_nombre.Size = new System.Drawing.Size(123, 22);
+            this.Txt_nombre.TabIndex = 118;
+            // 
+            // Txt_Codigo
+            // 
+            this.Txt_Codigo.Location = new System.Drawing.Point(251, 226);
+            this.Txt_Codigo.Name = "Txt_Codigo";
+            this.Txt_Codigo.Size = new System.Drawing.Size(123, 22);
+            this.Txt_Codigo.TabIndex = 117;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(657, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 17);
+            this.label4.TabIndex = 116;
+            this.label4.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 115;
+            this.label3.Text = "Codigo:";
+            // 
+            // Pnl_nombreForm
+            // 
+            this.Pnl_nombreForm.BackColor = System.Drawing.Color.Orange;
+            this.Pnl_nombreForm.Controls.Add(this.label2);
+            this.Pnl_nombreForm.Controls.Add(this.btn_Ayuda);
+            this.Pnl_nombreForm.Controls.Add(this.btn_minimizar);
+            this.Pnl_nombreForm.Controls.Add(this.label1);
+            this.Pnl_nombreForm.Controls.Add(this.btn_cerrar);
+            this.Pnl_nombreForm.Location = new System.Drawing.Point(-2, -3);
+            this.Pnl_nombreForm.Name = "Pnl_nombreForm";
+            this.Pnl_nombreForm.Size = new System.Drawing.Size(1293, 63);
+            this.Pnl_nombreForm.TabIndex = 114;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(13, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 85;
+            this.label2.Text = "102 - ";
+            // 
+            // btn_Ayuda
+            // 
+            this.btn_Ayuda.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Ayuda.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Ayuda.Image = ((System.Drawing.Image)(resources.GetObject("btn_Ayuda.Image")));
+            this.btn_Ayuda.Location = new System.Drawing.Point(903, 15);
+            this.btn_Ayuda.Name = "btn_Ayuda";
+            this.btn_Ayuda.Size = new System.Drawing.Size(44, 38);
+            this.btn_Ayuda.TabIndex = 6;
+            this.btn_Ayuda.UseVisualStyleBackColor = true;
+            // 
+            // btn_minimizar
+            // 
+            this.btn_minimizar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_minimizar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_minimizar.Image = ((System.Drawing.Image)(resources.GetObject("btn_minimizar.Image")));
+            this.btn_minimizar.Location = new System.Drawing.Point(856, 15);
+            this.btn_minimizar.Name = "btn_minimizar";
+            this.btn_minimizar.Size = new System.Drawing.Size(44, 38);
+            this.btn_minimizar.TabIndex = 5;
+            this.btn_minimizar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(86, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Curso";
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_cerrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_cerrar.Image = ((System.Drawing.Image)(resources.GetObject("btn_cerrar.Image")));
+            this.btn_cerrar.Location = new System.Drawing.Point(953, 15);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(44, 38);
+            this.btn_cerrar.TabIndex = 4;
+            this.btn_cerrar.UseVisualStyleBackColor = true;
+            this.btn_cerrar.Click += new System.EventHandler(this.Btn_cerrar_Click);
+            // 
+            // Btn_consultar
+            // 
+            this.Btn_consultar.BackColor = System.Drawing.Color.Orange;
+            this.Btn_consultar.FlatAppearance.BorderSize = 3;
+            this.Btn_consultar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.Btn_consultar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.Btn_consultar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.Btn_consultar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_consultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_consultar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_consultar.Image")));
+            this.Btn_consultar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_consultar.Location = new System.Drawing.Point(674, 88);
+            this.Btn_consultar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_consultar.Name = "Btn_consultar";
+            this.Btn_consultar.Size = new System.Drawing.Size(135, 110);
+            this.Btn_consultar.TabIndex = 113;
+            this.Btn_consultar.Text = "Consultar";
+            this.Btn_consultar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_consultar.UseVisualStyleBackColor = false;
+            this.Btn_consultar.Click += new System.EventHandler(this.Btn_consultar_Click);
+            // 
+            // Btn_borrar
+            // 
+            this.Btn_borrar.BackColor = System.Drawing.Color.Orange;
+            this.Btn_borrar.FlatAppearance.BorderSize = 3;
+            this.Btn_borrar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.Btn_borrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.Btn_borrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.Btn_borrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_borrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_borrar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_borrar.Image")));
+            this.Btn_borrar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_borrar.Location = new System.Drawing.Point(561, 88);
+            this.Btn_borrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_borrar.Name = "Btn_borrar";
+            this.Btn_borrar.Size = new System.Drawing.Size(115, 110);
+            this.Btn_borrar.TabIndex = 112;
+            this.Btn_borrar.Text = "Borrar";
+            this.Btn_borrar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_borrar.UseVisualStyleBackColor = false;
+            // 
+            // Btn_guardar
+            // 
+            this.Btn_guardar.BackColor = System.Drawing.Color.Orange;
+            this.Btn_guardar.FlatAppearance.BorderSize = 3;
+            this.Btn_guardar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.Btn_guardar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.Btn_guardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.Btn_guardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_guardar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_guardar.Image")));
+            this.Btn_guardar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_guardar.Location = new System.Drawing.Point(449, 88);
+            this.Btn_guardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_guardar.Name = "Btn_guardar";
+            this.Btn_guardar.Size = new System.Drawing.Size(115, 110);
+            this.Btn_guardar.TabIndex = 111;
+            this.Btn_guardar.Text = "Guardar";
+            this.Btn_guardar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_guardar.UseVisualStyleBackColor = false;
+            // 
+            // Btn_editar
+            // 
+            this.Btn_editar.BackColor = System.Drawing.Color.Orange;
+            this.Btn_editar.FlatAppearance.BorderSize = 3;
+            this.Btn_editar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.Btn_editar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.Btn_editar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.Btn_editar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_editar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_editar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_editar.Image")));
+            this.Btn_editar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_editar.Location = new System.Drawing.Point(337, 88);
+            this.Btn_editar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_editar.Name = "Btn_editar";
+            this.Btn_editar.Size = new System.Drawing.Size(115, 110);
+            this.Btn_editar.TabIndex = 110;
+            this.Btn_editar.Text = "Editar";
+            this.Btn_editar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_editar.UseVisualStyleBackColor = false;
+            // 
+            // Btn_ingresar
+            // 
+            this.Btn_ingresar.BackColor = System.Drawing.Color.Orange;
+            this.Btn_ingresar.FlatAppearance.BorderSize = 3;
+            this.Btn_ingresar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.Btn_ingresar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.Btn_ingresar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.Btn_ingresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_ingresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_ingresar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_ingresar.Image")));
+            this.Btn_ingresar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_ingresar.Location = new System.Drawing.Point(225, 88);
+            this.Btn_ingresar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_ingresar.Name = "Btn_ingresar";
+            this.Btn_ingresar.Size = new System.Drawing.Size(115, 110);
+            this.Btn_ingresar.TabIndex = 109;
+            this.Btn_ingresar.Text = "Ingresar";
+            this.Btn_ingresar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_ingresar.UseVisualStyleBackColor = false;
+            // 
+            // Frm_Mantcurso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 364);
+            this.Controls.Add(this.Cbo_estado);
+            this.Controls.Add(this.Estado);
+            this.Controls.Add(this.Txt_nombre);
+            this.Controls.Add(this.Txt_Codigo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Pnl_nombreForm);
+            this.Controls.Add(this.Btn_consultar);
+            this.Controls.Add(this.Btn_borrar);
+            this.Controls.Add(this.Btn_guardar);
+            this.Controls.Add(this.Btn_editar);
+            this.Controls.Add(this.Btn_ingresar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_Mantcurso";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_Mantcurso";
+            this.Pnl_nombreForm.ResumeLayout(false);
+            this.Pnl_nombreForm.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Cbo_estado;
+        private System.Windows.Forms.Label Estado;
+        private System.Windows.Forms.TextBox Txt_nombre;
+        private System.Windows.Forms.TextBox Txt_Codigo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel Pnl_nombreForm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Ayuda;
+        private System.Windows.Forms.Button btn_minimizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_cerrar;
+        private System.Windows.Forms.Button Btn_consultar;
+        private System.Windows.Forms.Button Btn_borrar;
+        private System.Windows.Forms.Button Btn_guardar;
+        private System.Windows.Forms.Button Btn_editar;
+        private System.Windows.Forms.Button Btn_ingresar;
+    }
+}
